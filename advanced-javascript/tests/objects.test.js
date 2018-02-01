32d8950d02e0124917369abf3107a7438cc589c7
Aaron Wilder <element1028@gmail.com>
2018-02-01 09:10:28 -0800

    submit

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -2,3 +2,12 @@ const assert = require('chai').expect;
 const objectFunctions = require('../src/objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+discribe('Objects', () => {
+  const testObject = {};
+  beforeEach(()=>{
+    
+  })
+  afterEach(()=> {
+
+  })
+})
--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -2,3 +2,27 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('getBiggest', ()=> {
+  const getBiggest = funcs.getBiggest;
+  it('should be a function', ()=> {
+    assert.typeOf(getBiggest, 'function');
+  });
+  it('should return a number', ()=> {
+    const testCall = getBiggest(10,20);
+    assert.typeOf(testCall,'number');
+  });
+  it('should return a larger number', ()=> {
+    const testCall = getBiggest(10,20);
+    assert.equal(testCall, 20);
+  });
+})
+
+describe('returnFirst', ()=> {
+  const returnFirst = funcs.returnFirst;
+  const myValue = [0,1,2,3]
+  it.skip('should return a specific value based on passed in array', () => {
+    const testCall = returnFirst(returnFirst);
+    assert.strictEquals(testCall, myValue);
+  });
+});
